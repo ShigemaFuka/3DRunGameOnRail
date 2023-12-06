fabd151b5c15c1d8a315d119e663083c0d7b9709
Shigema <tsubakyun4869@gmail.com>
2023-12-06 10:49:53 +0900

    [update] 微調整

--- Assets/Scripts/Rotation.cs
+++ Assets/Scripts/Rotation.cs
@@ -4,11 +4,6 @@ public class Rotation : MonoBehaviour
 {
     [SerializeField] float _rotationSpeed;
 
-    void Start()
-    {
-
-    }
-
     void Update()
     {
         transform.Rotate(Vector3.up * _rotationSpeed * Time.deltaTime, Space.World);
