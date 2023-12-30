9e3e8a6b92568cfecaaa7580f82a61243ef6012b
Shigema <tsubakyun4869@gmail.com>
2023-12-30 10:29:19 +0900

    [add] 無敵モード時のアニメーション

---
+++ Assets/Scripts/Invincible.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+/// <summary>
+/// 無敵状態のときに、UIとBGM再生
+/// </summary>
+public class Invincible : MonoBehaviour
+{
+    [SerializeField] AudioSource _audioSource = default;
+    [SerializeField,Tooltip("高くするピッチの値")] float _pitch = default;
+    [Tooltip("元々のピッチの値")] float _defaultPitch = default;
+    [SerializeField] Animator _animator = default;
+    void Start()
+    {
+        if(_audioSource) _defaultPitch = _audioSource.pitch;
+        _animator.SetBool("Invincible", false);
+    }
+
+    void Update()
+    {
+        if (GM.Instance._isInvincible && GM.Instance._inGame)
+        {
+            _audioSource.pitch = _pitch;
+            _animator.SetBool("Invincible", true);
+        }
+        else
+        {
+            _audioSource.pitch = _defaultPitch;
+            _animator.SetBool("Invincible", false);
+        }
+    }
+}
