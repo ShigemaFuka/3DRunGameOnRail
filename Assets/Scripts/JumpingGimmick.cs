5c27cd64a353b84d1ebabd50f052d5602f149e0e
Shigema <tsubakyun4869@gmail.com>
2024-01-08 16:38:25 +0900

    [add] 接触時に上前方にプレイヤーを吹き飛ばすギミック

---
+++ Assets/Scripts/JumpingGimmick.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+/// <summary>
+/// プレイヤーが接触してきたら、上前方に一瞬力を加える
+/// </summary>
+public class JumpingGimmick : MonoBehaviour
+{
+    [SerializeField, Tooltip("威力")] float _power = 20f;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player") || other.gameObject.CompareTag("Invincible"))
+        {
+            other.gameObject.GetComponent<Rigidbody>().AddForce((Vector3.up + Vector3.forward) * _power, ForceMode.Impulse);
+            //音とアニメーションを追加したい
+        }
+    }
+}
